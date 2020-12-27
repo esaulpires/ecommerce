51baf5f6616438bae94ea09af3486de66780360c
Esaul Pires <esaul.suporte@outook.com>
2020-12-27 18:55:25 -0300

    Listagem de produtos - OK

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php 
+
+use \Hcode\Model\User;
+
+
+function formatPrice(float $vlprice)
+{
+
+	if (!$vlprice > 0) $vlprice = 0;
+
+	return number_format($vlprice, 2, ",", ".");
+
+}
+
+
+ ?>
--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -9,7 +9,7 @@ use PHPMailer\PHPMailer\Exception;
 class Mailer {
 	
 	const USERNAME = "informaticaworkflow@gmail.com";
-	const PASSWORD = "*********";
+	const PASSWORD = "TestandoEmail";
 	const NAME_FROM = "Workflow Informatica";
 
 	private $mail;
